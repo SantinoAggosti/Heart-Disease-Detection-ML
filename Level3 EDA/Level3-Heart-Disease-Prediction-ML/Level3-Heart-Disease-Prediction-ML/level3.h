4be4409b61f750136142b67f67661f6fce87542a
SantinoAggosti <97355841+SantinoAggosti@users.noreply.github.com>
2023-04-29 09:25:32 -0300

    First Commit

---
+++ Level3 EDA/Level3-Heart-Disease-Prediction-ML/Level3-Heart-Disease-Prediction-ML/level3.h
@@ -0,0 +1,8 @@
+﻿// Level3-Heart-Disease-Prediction-ML.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
---
+++ Level3 EDA/Level3-Heart-Disease-Prediction-ML/Level3-Heart-Disease-Prediction-ML/main.cpp
@@ -0,0 +1,12 @@
+﻿// Level3-Heart-Disease-Prediction-ML.cpp : Defines the entry point for the application.
+//
+
+#include "Level3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
