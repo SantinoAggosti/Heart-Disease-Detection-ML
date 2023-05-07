bb2a5725518747604289bdcb51d638e25f4f3754
SantinoAggosti <97355841+SantinoAggosti@users.noreply.github.com>
2023-05-07 14:59:22 -0300

    Friday updates
    Mejoras en clase de Dataset y definicion de funciones para el manejo de la estructura de datos.

--- Level3Eda-HeartDiseaseDetection/Level3Eda-HeartDiseaseDetection/dataSet.cpp
+++ Level3Eda-HeartDiseaseDetection/Level3Eda-HeartDiseaseDetection/dataSet.cpp
@@ -1,16 +1,73 @@
 #include "dataSet.h"
 #include "CSVData.h"
 
-void createDataStructure(CSVData csvData, DataSet &dataSet) {
-	for (auto column : csvData){
+void createDataStructure(CSVData& csvData, DataSet& dataSet) {
+	char dataValue = 0;
+
+	// Intento de guardar los outputs de cada ejemplo en un arreglo-
+	//vector<string> output;
+	//for (const auto& row : csvData) {
+	//	if (row.size() >= dataSet.numberOfFields) {
+	//		output.push_back(row[dataSet.numberOfFields]);
+	//	}
+	//}
+
+	for (auto column : csvData) {
+		if (column[0] == "output") {
+			break;
+		}
 		for (auto row : column) {
-			// POR CADA COLUMNA, GUARDAR LA INFORMACION EN SU RESPECTIVA POSICION EN EL DATA SET
+			Field field = dataSet.dataSet_fields.find(column[0])->second;
+			dataValue = restrictValue(dataSet, stoi(row), field);
+
+			Subset subset = field.field_values.find(dataValue)->second;
+			//Se guarda la informacion en su respectiva posicion.
+			//subset.subset.insert(make_pair())
 		}
 	}
 }
 
-void assignDataSetValue(DataSet &dataSet, char value) {
-	for (string fieldName : dataSet.fieldNames) {
-		//SWITCH STATEMENT COMPARANDO LOS FIELD NAMES CON LOS POSIBLES RESULTADOS Y REASIGNAR EL VALUE
+char restrictValue(DataSet& dataSet, char value, Field field) {
+	switch (dataSet.fieldNames.find(field.fieldName)->second) {
+		//AGE
+	case 0:
+		if (value < 50) {
+			return 0;
+		}
+		else if (value < 65) {
+			return 1;
+		}
+		return 2;
+		break;
+		//trtbps
+	case 3:
+		if (value < 120) {
+			return 0;
+		}
+		return 1;
+		break;
+		//Chol
+	case 4:
+		if (value < 240) {
+			return 0;
+		}
+		return 1;
+		break;
+		//Thalach
+	case 7:
+		if (value < 150) {
+			return 0;
+		}
+		return 1;
+		break;
+		//Oldpeak
+	case 9:
+		if (value < 1) {
+			return 0;
+		}
+		return 1;
+		break;
 	}
+
+	return 0;
 }
--- Level3Eda-HeartDiseaseDetection/Level3Eda-HeartDiseaseDetection/dataSet.h
+++ Level3Eda-HeartDiseaseDetection/Level3Eda-HeartDiseaseDetection/dataSet.h
@@ -6,11 +6,11 @@
 class DataSet {
 public:
 	DataSet() {
-		fieldNames = { "age", "sex", "cp", "trtbps", "chol", "fbs", "restecg", "thalachh", "exng", "oldpeak", "slp", "caa", "thall"};
+		fieldNames = { {"age",0},{"sex",1},{"cp",2}, {"trtbps",3}, { "chol",4 }, { "fbs",5 }, { "restecg",6 }, { "thalachh",7 }, { "exng",8 }, { "oldpeak",9 }, { "slp",10 }, { "caa",11 }, { "thall",12 } };
 		char counter = 0;
 		for (auto &fieldName : fieldNames) {
 			counter++;
-			dataSet_fields.insert(make_pair(fieldName, Field(fieldName)));
+			dataSet_fields.insert(make_pair(fieldName.first, Field(fieldName.first)));
 
 			// ASIGNAR LOS POSIBLES VALORES DE CADA FIELD
 
@@ -18,10 +18,11 @@ public:
 		numberOfFields = counter - 1;
 	}
 	char numberOfFields;
-	vector<string> fieldNames;
+	unordered_map<string, char> fieldNames;
 	unordered_map<string, Field> dataSet_fields;
 };
 
 void createDataStructure();
 void assignDataSetValue(DataSet& dataSet, char value);
+char restrictValue(DataSet& dataSet, char value, Field field);
 #endif
